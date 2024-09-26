3ea6d95ecd5cb7445fecfb4125fbb9bdc935e56b
Sfsprecher <sfsprecher@miners.utep.edu>
2024-09-26 10:55:57 -0600

    9/26

--- CLass.py
+++ CLass.py
@@ -3,21 +3,13 @@ import pandas as py
 filename = "./big-mac-full-index.csv"
 bm = py.read_csv(filename)
 
-# print(bm)
+print(bm.columns)
 
-# print(bm.columns)
+print(bm.index)
 
-# query = f"(iso_a3 == 'MEX')"
+for i in bm.index:
 
-# mxn_df = bm.query(query)
+    #print(bm["name"][i])
 
-
-# print(mxn_df)
-
-query = f"(iso_a3 == 'MEX')"
-
-mxn_df = bm.query(query)
-
-min_idx = mxn_df['dollar price'].idxmin()
-
-print(min_idx)
+    for index, row in bm.iterrows():
+        print(bm['name'][index])
--- code_4.py
+++ code_4.py
@@ -5,7 +5,7 @@ big_mac_file = './big-mac-full-index.csv'
 bm = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    
+    query = f"(iso_a3,date)"
 
 def get_big_mac_price_by_country(country_code):
     cc = country_code.upper()
@@ -15,11 +15,15 @@ def get_big_mac_price_by_country(country_code):
     return round (bm_by_country["dollar_price"].mean(),2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    
-    
+
+    query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
+    min_bm = bm.query(query_text)
+    min_idx= min_bm["dollar_price"].idxmin()
+    return round (query_text["dollar_price"])
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
    
+   get_the_cheapest_big_mac_price_by_year(2010)
