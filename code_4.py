0dab524892078397e5fcd85a350a1769d0d9622a
Sfsprecher <sfsprecher@miners.utep.edu>
2024-09-29 20:38:09 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -9,21 +9,25 @@ def get_big_mac_price_by_year(year,country_code):
 
 def get_big_mac_price_by_country(country_code):
     cc = country_code.upper()
-    # Creates logic for our country code told by tutor I should use .upper() as it will work with github grade better 
+    # Creates logic for our country code to all be in UPPER CASE told by tutor I should use .upper() as it will work with github grade better 
     bm_by_country = bm[bm["iso_a3"] == country_code] 
-    # 
+    # sets our iso_a3 equal to contry code in our big-mac-index
     return round (bm_by_country["dollar_price"].mean(),2)
-
+    # returns the dollar price mean for the country_code that is entdfdc
 def get_the_cheapest_big_mac_price_by_year(year):
 
     query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
     min_bm = bm.query(query_text)
     min_idx= min_bm["dollar_price"].idxmin()
-    return round (query_text["dollar_price"])
-
+    return round (min_idx['name']["dollar_price"])
+                    
+    
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
+   year = 2010
    
-   get_the_cheapest_big_mac_price_by_year(2010)
+   get_the_cheapest_big_mac_price_by_year(year)
+
+   
