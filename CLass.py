741aee23f8eec5e5fa04f09938133dbd56c3be2f
Sfsprecher <sfsprecher@miners.utep.edu>
2024-09-29 19:15:42 -0600

    Update CLass.py

--- CLass.py
+++ CLass.py
@@ -3,13 +3,30 @@ import pandas as py
 filename = "./big-mac-full-index.csv"
 bm = py.read_csv(filename)
 
-print(bm.columns)
+# print(bm.columns)
 
-print(bm.index)
+# print(bm.index)
 
 for i in bm.index:
+    print(bm["name"][i])
+for i in bm.index:
 
-    #print(bm["name"][i])
+    print(bm.loc[i]['name'])
+for i in bm.index:
 
-    for index, row in bm.iterrows():
-        print(bm['name'][index])
+    print(bm.iloc[i]['name'])
+
+for index, row in bm.iterrows():
+    print(bm['name'][index])
+# for index, row in bm.iterrows():
+    #print(bm['name'][index])
+
+
+
+#def get_new_country_name(row):
+   # print(row)
+   # new_country_name = f"{row['name']}({row['iso_a3']})"
+   # print(new_country_name)
+   # bm.apply(get_new_country_name,axis=1)
+   # return new_country_name
+# Was doing the apply function wrong somehow on this last section
