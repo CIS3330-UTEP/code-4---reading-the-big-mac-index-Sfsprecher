f8e1ffbf71c545bd3e67734d44bcf93dcbdfee3c
Sfsprecher <sfsprecher@miners.utep.edu>
2024-09-29 21:20:00 -0600

    9/29 max and min done

--- code_4.py
+++ code_4.py
@@ -18,19 +18,32 @@ def get_the_cheapest_big_mac_price_by_year(year):
 
     query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
     min_bm = bm.query(query_text)
+    # this creates are query for the year that is entered using the dates within each year
     min_idx= min_bm["dollar_price"].idxmin()
+    # Identifies the row with the lowest minimum for that row
     price_min = min_bm.loc[min_idx, "dollar_price"]
+    # Finds the dollar price for that specific row using the loc 
+    # I did ask CHAT gpt "How am I setting this line up wrong?" with my orignal line here without the loc.
     price_min_rounded = round (price_min,2)
+    # uses for rounding the price to 2 decimals
     country_name = min_bm.loc[min_idx, "name"]
+    # finds the country name for that row
     country_3 = min_bm.loc[min_idx, "iso_a3"]
-    print(f"{country_name}({country_3}): ${price_min_rounded}")
-
+    # finds the 3 letter 
+    print(f"{country_name}({country_3}): ${price_min_rounded}'")
+    # I know this is a very ugly long code but I was have trouble doing it the way I think it should be done so I improvised
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
-
+    query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
+    max_bm = bm.query(query_text)
+    max_idx= max_bm["dollar_price"].idxmax()
+    price_max = max_bm.loc[max_idx, "dollar_price"]
+    price_max_rounded = round (price_max,2)
+    country_name = max_bm.loc[max_idx, "name"]
+    country_3 = max_bm.loc[max_idx, "iso_a3"]
+    print(f"{country_name}({country_3}): ${price_max_rounded}'")
 if __name__ == "__main__":
-   year = 2010
+   year = 2003
    
-   get_the_cheapest_big_mac_price_by_year(year)
+   get_the_most_expensive_big_mac_price_by_year(year)
 
    
