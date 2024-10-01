1e1a5cf6dc29d86e9fba6c4cf131732b77279431
Sfsprecher <sfsprecher@miners.utep.edu>
2024-10-01 14:01:07 -0600

    10/1/24

--- code_4.py
+++ code_4.py
@@ -5,18 +5,28 @@ big_mac_file = './big-mac-full-index.csv'
 bm = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
-    bm_by_country = bm[bm["iso_a3"] == country_code]
-    # Having trouble combining them
-    return round (bm_by_country["dollar_price"].mean(),2)
-
+    query_text = f"((iso_a3 == '{country_code}'and date >= '{year}-01-01' and date <= '{year}-12-31'))"
+    # Query for 2 different filters the country code and the tear
+    bm_m = bm.query(query_text)
+    price_by_year= bm_m['dollar_price'].mean()
+    # Finds the dollar price mean using the query
+    price_by_year_rounded = round (price_by_year,2)
+    # rounds the price by 2 decimal places
+    return (price_by_year_rounded)
+    
 def get_big_mac_price_by_country(country_code):
     cc = country_code.upper()
     # Creates logic for our country code to all be in UPPER CASE told by tutor I should use .upper() as it will work with github grade better 
-    bm_by_country = bm[bm["iso_a3"] == country_code] 
-    # sets our iso_a3 equal to contry code in our big-mac-index
-    return round (bm_by_country["dollar_price"].mean(),2)
-    # returns the dollar price mean for the country_code that is entdfdc
+    query_text = f"(iso_a3 == '{country_code}')"
+    # Filter for the country_code
+    bm_m = bm.query(query_text)
+    price_by_country = bm_m['dollar_price'].mean()
+    # Finds the mean using our query
+    price_by_country_rounded = round (price_by_country,2)
+    # Rounds price to two decimal places
+    return (price_by_country_rounded)
+    
+
 def get_the_cheapest_big_mac_price_by_year(year):
 
     query_text = f"((date >= '{year}-01-01' and date <= '{year}-12-31'))"
@@ -33,7 +43,7 @@ def get_the_cheapest_big_mac_price_by_year(year):
     # finds the country name for that row
     country_3 = min_bm.loc[min_idx, "iso_a3"]
     # finds the 3 letter 
-    print(f"{country_name}({country_3}): ${price_min_rounded}'")
+    return (f"{country_name}({country_3}): ${price_min_rounded}'")
     # I know this is a very ugly long code but I was have trouble doing it the way I think it should be done so I improvised
 def get_the_most_expensive_big_mac_price_by_year(year):
     #Follows same code above instead finding the max instead of the min
@@ -44,11 +54,12 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     price_max_rounded = round (price_max,2)
     country_name = max_bm.loc[max_idx, "name"]
     country_3 = max_bm.loc[max_idx, "iso_a3"]
-    print(f"{country_name}({country_3}): ${price_max_rounded}'")
+    return (f"{country_name}({country_3}): ${price_max_rounded}'")
     
 if __name__ == "__main__":
-   
-   
+ pass
+       
+
    
    
 
